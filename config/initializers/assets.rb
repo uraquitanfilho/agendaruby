73a7f24b793b729ac95a1dc4c88718c6d77b0e8d
uraquitan.matova <uraquitan.matova@xposeutoo.com>
2017-12-21 06:14:32 -0500

    Assets Pipiline - 55

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,18 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+##adicionado para testar inclução de um js criado pelo usuário
+#deverá aplicar tb em app/views/layouts/application.html.erb a linha:
+#<%= javascript_include_tag 'alert'%>
+Rails.application.config.assets.precompile += %w( alert.js
+                                                  addresses.js
+                                                  contacts.js
+                                                  home.js
+                                                  kinds.js
+                                                  phones.js
+                                                  addresses.css
+                                                  contacts.css
+                                                  home.css
+                                                  kinds.css
+                                                  phones.css)
